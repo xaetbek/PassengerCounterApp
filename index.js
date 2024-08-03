e9b7c8afccdcbfe3bd688c1f65824dad855075d9
xaetbek <xaetbek@gmail.com>
2024-08-03 11:22:37 -0700

    Pax Counter App

---
+++ index.js
@@ -0,0 +1,18 @@
+//document.getElementById("count-el").innerText = 5
+
+// let myAge = 27
+// let humanDogRatio = 7
+// let myDogAge = myAge * humanDogRatio
+
+// console.log(myDogAge)
+
+
+let bonusPoints = 50 
+bonusPoints = bonusPoints + 50
+bonusPoints = bonusPoints - 25
+bonusPoints = bonusPoints + 70
+
+console.log(bonusPoints)
+
+
+
